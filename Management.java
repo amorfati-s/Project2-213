502f9fc00013f77488b06f733ab7c4cfd662c5ed
Sonal <57592260+amorfati-s@users.noreply.github.com>
2021-02-21 14:22:47 -0500

    updated profile to work in class, calcPay, toString, equals

--- Management.java
+++ Management.java
@@ -1,4 +1,53 @@
+import java.text.DecimalFormat;
+
 public class Management extends Fulltime {
+
+    private int role;
+    private static final int MANAGER = 1;
+    private static final int DEPT_HEAD = 2;
+    private static final int DIRECTOR = 3;
+    private double additionalComp;
+
+    public Management(Profile profile, double annualSalary, int role) { //might have to change Date to a string ?? idk if it'll work
+        super(profile, annualSalary);
+        this.role = role;
+        if (role == 1) {
+            additionalComp = 192.31;
+        } else if (role == 2) {
+            additionalComp = 365.38;
+        } else if (role == 3) {
+            additionalComp = 461.54;
+        }
+    }
+
+    public void calculatePayment() {
+        super.calculatePayment();
+        payment = payment + additionalComp;
+    }
+
     @Override
-    public void calculatePayment() { }
+    public String toString() {
+        return super.toString() + "::Manager Compensation $" + additionalComp;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Management) {
+            Management management = (Management) obj;
+            return management.getProfile().getName().equals(this.getProfile().getName());
+
+        }
+        return false;
+    }
 }
+    /*
+        public Management(String manager, String deptHead, String director) {
+        super(manager, deptHead, director);
+    }
+
+    @Override
+    public String toString() {
+        return ("Manager: " + manager + "department head" + deptHead + "director" + director);
+    }
+
+     */
