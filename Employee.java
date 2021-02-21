df920076be7bc6a095986b923fc752b09e0b1c69
Sonal <57592260+amorfati-s@users.noreply.github.com>
2021-02-21 14:21:09 -0500

    updated

--- Employee.java
+++ Employee.java
@@ -1,6 +1,35 @@
 public class Employee {
+    private Profile profile;
+    private double payment;
 
+    public Employee() {
 
+    }
 
-    public void calculatePayment() { }
+    public Employee(Profile profile) {
+        this.profile = profile;
+        this.payment = 0;
+    }
+
+    public Profile getProfile() {
+        return this.profile;
+    }
+
+    public void calculatePayment() {
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Employee) {
+            Employee employee = (Employee) obj;
+            return employee.profile.equals(this.profile);
+        }
+        return false;
+    }
+
+    public String toString() {
+        return profile.toString();
+    }
+
 }
