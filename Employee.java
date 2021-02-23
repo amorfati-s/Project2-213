b918228bec43a3c4d353029fe00fec6531c9dbce
Sonal <57592260+amorfati-s@users.noreply.github.com>
2021-02-23 13:53:43 -0500

    updated code for testing

--- Employee.java
+++ Employee.java
@@ -11,8 +11,12 @@ public class Employee {
         this.payment = 0;
     }
 
+
     public Profile getProfile() {
         return this.profile;
+    }
+    public void setProfile(Profile profile) {
+        this.profile = profile;
     }
 
     public void calculatePayment() {
@@ -31,5 +35,5 @@ public class Employee {
     public String toString() {
         return profile.toString();
     }
- 
+
 }
